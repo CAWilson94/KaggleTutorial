0a3f726b7b4fb4f4158a5787667e3203c96d5ddd
CAWilson94 <charlotteawilson.94@gmail.com>
2015-12-04 12:48:10 +0000

    Tutorial data cleaned up

--- tutorial.py
+++ tutorial.py
@@ -10,3 +10,7 @@ import pandas as pd
 titanic = pd.read_csv("train.csv")
 print (titanic.head(5)) # Print first 5 rows of csv
 print (titanic.describe()) # Age only shows 714 rows, others are 891. Need to clean data!
+
+titanic["Age"] = titanic["Age"].fillna(titanic["Age"].median()) # Fill in empty cells with median of all 
+
+print (titanic.describe())
