1cc8e59a591a1d8edd6ee599cd0c77d847fdc8bd
CAWilson94 <charlotteawilson.94@gmail.com>
2015-12-04 12:44:25 +0000

    started data clean up in tutorial code

--- tutorial.py
+++ tutorial.py
@@ -5,14 +5,8 @@ Going through Kaggle tutorial
 """
 import csv as csv
 import numpy as mp
+import pandas as pd
 
-csvFile = csv.reader(open('train.csv','rb'))
-# We have a header line, so need to skip this!
-headerSkip = csvFile.next()
-# Shove the data into array (would normally use bunch object)
-data = []
-
-# Could normally use the pandas library to do this automatically ...
-for row in csvFile     # Loop through each row
-	data.append(row)   # Add each row to the data variable
-data = np.array(data)  # convert from List to Array
+titanic = pd.read_csv("train.csv")
+print (titanic.head(5)) # Print first 5 rows of csv
+print (titanic.describe()) # Age only shows 714 rows, others are 891. Need to clean data!
