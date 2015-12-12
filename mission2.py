21267df1166dd226f1e167c76d592eb4f93a9ed3
CAWilson94 <charlotteawilson.94@gmail.com>
2015-12-12 15:26:12 +0000

    Mission 2 started, improving the accuracy of the model/submission

---
+++ mission2.py
@@ -0,0 +1,16 @@
+"""
+To improve the accuracy of my submission, 
+using the dataquest titanic tutorial
+"mission 2"
+
+Author @Charlotte Alexandra Wilson
+Data   December 2015
+
+"""
+
+# Since our submission wasn't very high scoring, there are 3 ways
+# in which we can improve it:
+# (1) Use a better machine learning algorithm
+# (2) Generate better features
+# (3) Combine multiple machine learning algorithms (this is interesting...)
+
